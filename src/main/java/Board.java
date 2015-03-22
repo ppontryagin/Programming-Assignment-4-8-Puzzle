3cf391c973968c28ef153833237d49618bae6070
Pavel.Pontryagin <Pavel.Pontryagin@billing.ru>
2015-03-22 10:21:24 +0300

    implemented Board.hamming, Board.manhattan

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -3,32 +3,58 @@
  */
 public class Board {
 
-    private int[][] tiles;
+    private int[] tiles;
     private int N;
 
     // construct a board from an N-by-N array of blocks
     // (where blocks[i][j] = block in row i, column j)
     public Board(int[][] blocks) {
-        this.tiles = blocks;
         this.N = blocks.length;
+        tiles = new int[N * N + 1];
+        int n = 1;
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++) {
+                tiles[n] = blocks[i][j];
+                n++;
+            }
     }
 
     // board dimension N
     public int dimension() {
-        return tiles.length;
+        return N;
     }
 
-    //TODO number of blocks out of place
+    // number of blocks out of place
     public int hamming() {
-        return 0;
+        int n = 0;
+
+        for (int i = 1; i <= N * N; i++) {
+            if (tiles[i] != i && tiles[i] != 0) {
+                n++;
+            }
+        }
+        return n;
     }
 
-    //TODO  sum of Manhattan distances between blocks and goal
+    // sum of Manhattan distances between blocks and goal
     public int manhattan() {
-        return 0;
+        int n = 0;
+        Point2D curr;
+        Point2D targ;
+
+        for (int i = 1; i <= N * N; i++) {
+            if (tiles[i] != 0) {
+                curr = new Point2D(i, N);
+                targ = new Point2D(tiles[i], N);
+                n += targ.compare(curr);
+            }
+        }
+
+        return n;
     }
 
     //TODO  is this board the goal board?
+
     public boolean isGoal() {
         return false;
     }
@@ -52,17 +78,49 @@ public class Board {
     public String toString() {
         StringBuilder s = new StringBuilder();
         s.append(N + "\n");
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                s.append(String.format("%2d ", tiles[i][j]));
-            }
-            s.append("\n");
+        for (int i = 1; i < N * N + 1; i++) {
+            s.append(String.format("%2d ", tiles[i]));
+            if (i % N == 0)
+                s.append("\n");
         }
+
         return s.toString();
     }
 
     // unit tests (not graded)
     public static void main(String[] args) {
 
+    }
+
+    private class Point2D {
+        public int x;
+        public int y;
+
+        private Point2D(int p, int dim) {
+            if ((p % dim) > 0) {
+                y = p / dim + 1;
+                x = p % dim;
+
+            } else {
+                y = p / dim;
+                x = dim;
+            }
+        }
+
+        @Override
+        public String toString() {
+            return "Point2D{" +
+                    "x=" + x +
+                    ", y=" + y +
+                    '}';
+        }
+
+        public boolean equals(Point2D that) {
+            return compare(that) == 0;
+        }
+
+        public int compare(Point2D that) {
+            return Math.abs(that.x - this.x) + Math.abs(that.y - this.y);
+        }
     }
 }
--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -1,23 +1,60 @@
-import org.junit.*;
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by Pavel.Pontryagin on 21.03.2015.
  */
 public class BoardTest {
 
-    @Test
-    public void soutIsCorrect() {
+    Board board;
+    Board boardRev;
 
-        Board board = new Board(makeTiles(3));
+
+    @Before
+    public void setUp() {
+        board = new Board(makeTiles(3));
+        boardRev = new Board(makeTilesReverse(3));
+    }
+
+    @Test
+    public void testHamming() {
+        assertThat(board.hamming(), is(0));
+        assertThat(boardRev.hamming(), is(8));
+    }
+
+    @Test
+    public void testManhattan() {
+        assertThat(board.manhattan(), is(0));
+        assertThat(boardRev.manhattan(), is(16));
+    }
+
+    @Ignore
+    public void soutIsCorrect() {
         StdOut.println(board);
+        StdOut.println(boardRev);
     }
 
     private int[][] makeTiles(int N) {
         int[][] blocks = new int[N][N];
         for (int i = 0; i < N; i++)
             for (int j = 0; j < N; j++)
-                blocks[i][j] = i + j;
+                blocks[i][j] = i * N + j + 1;
+
+        blocks[N - 1][N - 1] = 0;
+
+        return blocks;
+    }
+
+    private int[][] makeTilesReverse(int N) {
+        int[][] blocks = new int[N][N];
+        int k = 8;
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                blocks[i][j] = k--;
 
         return blocks;
     }
